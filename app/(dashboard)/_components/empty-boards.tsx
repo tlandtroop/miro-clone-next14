d353317d102aeee96004dc650378520e3c355ce4
Tyler Landtroop <tlandtroop@ufl.edu>
2024-03-03 20:38:08 -0500

    search querying

--- app/(dashboard)/_components/empty-boards.tsx
+++ app/(dashboard)/_components/empty-boards.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 import { useOrganization } from "@clerk/nextjs";
 import { toast } from "sonner";
 
@@ -9,6 +10,7 @@ import { api } from "@/convex/_generated/api";
 import { useApiMutation } from "@/hooks/use-api-mutation";
 
 export const EmptyBoards = () => {
+  const router = useRouter();
   const { organization } = useOrganization();
   const { mutate, pending } = useApiMutation(api.board.create);
 
@@ -21,6 +23,7 @@ export const EmptyBoards = () => {
     })
       .then((id) => {
         toast.success("Board created!");
+        router.push(`/board/${id}`);
       })
       .catch(() => toast.error("Failed to create board!"));
   };
